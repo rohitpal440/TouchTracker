a611902137caafdd12ca27e2d084129e5982b706
Rohit Pal <rohitpal440@gmail.com>
2018-01-22 02:25:20 +0530

    Add way to stroke line in DrawView

--- TouchTracker/DrawView.swift
+++ TouchTracker/DrawView.swift
@@ -9,13 +9,31 @@
 import UIKit
 
 class DrawView: UIView {
+    var currentLine: Line?
+    var finishedLines: [Line] = []
 
-    /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
         // Drawing code
+        UIColor.black.setStroke()
+        for line in finishedLines {
+            strokeLine(line: line)
+        }
+        if let line = currentLine {
+            // if line is currently being drawn, do it in green
+            UIColor.green.setStroke()
+            strokeLine(line: line)
+        }
     }
-    */
+
+    func strokeLine(line: Line) {
+        let path = UIBezierPath()
+        path.lineWidth = 10;
+        path.lineCapStyle = .round
+        path.move(to: line.begin)
+        path.addLine(to: line.end)
+        path.stroke()
+    }
 
 }
