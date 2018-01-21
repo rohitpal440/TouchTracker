8ee143c63f4d86170535fdea4a3bf5785ca76557
Rohit Pal <rohitpal440@gmail.com>
2018-01-22 02:20:21 +0530

    Create a struct Line

---
+++ TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Rohit Pal on 22/01/18.
+//  Copyright © 2018 Technobells. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin: CGPoint = .zero
+    var end: CGPoint = .zero
+}
